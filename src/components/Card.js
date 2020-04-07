9cdd5fa237620dff747f400eb7ce587ccd7083ba
Cook1619 <cook6348@gmail.com>
2020-04-06 20:05:18 -0500

    brinign in sass

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Card extends Component {
+    render() {
+        return (
+            <div className="github-profile">
+                One Github Profile
+            </div>
+        )
+    }
+}
+
+export default Card
